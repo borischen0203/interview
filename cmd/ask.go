dfa4bb7459e13572e942c20e43b869853c4f422b
Boris Chen <boris@Boris-Chen.local>
2021-08-30 18:28:39 +0800

    UPD. update get command

--- cmd/ask.go
+++ cmd/ask.go
@@ -47,9 +47,10 @@ var askCmd = &cobra.Command{
 
 //Load csv file with default interview questions
 func Load() (map[int]string, error) {
-	file, err := os.Open("cmd/default.csv")
+	file, err := os.Open("default.csv")
 
 	if err != nil {
+		fmt.Println("An error encountered ::", err)
 		log.Fatal(err)
 	}
 	defer file.Close()
